8640e343610ab7904b4ce00e9e61a2987a3cefa8
Vinay Vyas <vbvyas@gmail.com>
2014-05-10 21:43:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var Hapi = require('hapi');
 var server = Hapi.createServer('0.0.0.0', parseInt(process.env.PORT, 10) || 3000);
+
+server.route({
+  method: 'GET',
+  path: '/'
+  handler: function (req, res) {
+    res('app is running');
+  }
+});
