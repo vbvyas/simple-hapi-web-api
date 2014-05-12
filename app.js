2f617819e565d253454e89cec1133623becc4e7a
Vinay Vyas <vbvyas@gmail.com>
2014-05-11 21:36:33 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,21 @@ server.route({
   }
 });
 
+var test = [
+  {
+    "id": 1,
+    "data": "test"
+  },
+  {
+    "id": 2,
+    "data": "test"
+}];
+
 server.route({
   method: 'GET',
   path: '/test',
   handler: function (req, res) {
-    res();
+    res(test);
   }
 })
 
